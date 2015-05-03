85a120ac936e8c91ee82b8986b3b64e5626dd934
Walid <walidvb@gmail.com>
2015-05-03 05:28:18 +0200

    updates page and styles

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(function(){
-  // Library declaration
   function Library(obj, i){
     this.id = i;
     this.name = obj.name;
@@ -14,10 +13,10 @@ $(function(){
       that.more.append($('<div class="desc">').text(that.proj.description));
       var sidebar = $('<div class="sidebar">');
       if(that.proj.homepage){
-        sidebar.append('<div class="homepage"><strong>website:</strong><a href="'+ that.proj.homepage+'" target="_blank">'+ that.proj.homepage +'</a></div>');
+        sidebar.append($('<div class="homepage"><strong>homepage:</strong><a href="'+ that.proj.homepage+'" target="_blank">'+ that.proj.homepage +'</a></div>'));
       }
-      var filesWrapper = $('<div class="files">')
-      filesWrapper.append('<div class="files-header">Files:</div>');
+      var filesWrapper = $('<div class="files">');
+      filesWrapper.append($('<div class="files-header">Files:</div>'));
       var files = that.proj.assets[0].files;
       for(var i = 0; i < files.length; i++){
         file = files[i];
@@ -69,39 +68,32 @@ $(function(){
     }
     if(!this.$)
     {
-      this.$ = $('<li data-id="'+this.id+'" class="lib"></li>').append("<div class='header'><div class='name'>"+this.name+"</div><a class='more-info'>more</a><a class='add add-library'>add latest</a></div>");
+      this.$ = $('<li data-id="'+this.id+'" class="lib"></li>').append($("<div class='header'><div class='name'>"+this.name+"</div><a class='more-info'>more</a><a class='add add-library'>add latest</a></div>"));
     }
     this.displayed = true;
   };
   
-
   var apiUrl = "http://api.cdnjs.com/libraries";
-  var wrapper;
-  var libsContainer;
-  var searchUI;
-  var libs;
+  var wrapper, libsContainer, searchUI, libs;
   
-  // events binding
-  
-
-  // logic
   $.get(apiUrl, function(data){
     libs = [];
     for(var i = 0; i < data.results.length; i++){
       libs.push(new Library(data.results[i], i));
       libs[i].display(true);
-    }  
+    }
     updateView();
   });
   
   function init(){
     wrapper = $('<div class="w-wrapper">');
     searchUI = $('<input type="text" name="search" placeholder="type in a library">');
-    wrapper.append(searchUI);
-    libsContainer = $('<ul id="libraries">')
-    wrapper.append(libsContainer);
     wrapper.appendTo($('body'));
-
+    wrapper.append(searchUI);
+    wrapper.append($('<div class="close">X</div>'));
+    libsContainer = $('<ul id="libraries">').text('loading');
+    wrapper.append(libsContainer);
+    searchUI.focus();
     searchUI.on('keyup', function(e){
       var query = $(this).val();
       if(query.length >= 1)
@@ -130,7 +122,6 @@ $(function(){
     });
     $(document).on('click', '.w-wrapper .add-file', function(e){
       var id = $(e.target).parents('[data-id]').data('id');
-      console.log("e.target:", e.target);
       $(e.target).animate({'opacity': 0});
       libs[id].addFile($(e.target).data('index'));
     });
@@ -139,38 +130,51 @@ $(function(){
         wrapper.remove();
       });
     });
+    $(document).on('click', function(e){
+      var parent = $(e.target).parents('.w-wrapper');
+      if(parent.length < 1 || $(e.target).hasClass('close'))
+      {
+        wrapper.remove();
+      }
+    });
   };
   init();
   function updateView(){
-    var rows = [];
+    var rows = $('<ul id="#library">');
     for(var i = 0; i < libs.length; i++)
     {
       if(libs[i].displayed){
-        rows.push(libs[i].$);
+        rows.append(libs[i].$);
       }
     }
     libsContainer.html(rows);
   };
   
   function loadjscssfile(filename){
-    if (/(js)$/.test(filename)){ //if filename is a external JavaScript file
-        var fileref=document.createElement('script')
-        fileref.setAttribute("type","text/javascript")
-        fileref.setAttribute("src", filename)
+    console.log("loaded ", filename);
+    if (/(js)$/.test(filename)){ 
+        var fileref=document.createElement('script');
+        fileref.setAttribute("type","text/javascript");
+        fileref.setAttribute("src", filename);
     }
-    else if (/(css)$/.test(filename)){ //if filename is an external CSS file
-        var fileref=document.createElement("link")
-        fileref.setAttribute("rel", "stylesheet")
-        fileref.setAttribute("type", "text/css")
-        fileref.setAttribute("href", filename)
+    else if (/(css)$/.test(filename)){ 
+        var fileref=document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
     }
     if (typeof fileref!="undefined")
     {
-        document.getElementsByTagName("head")[0].appendChild(fileref)
+        document.getElementsByTagName("head")[0].appendChild(fileref);
     }
     else{
       console.log("an error has occured when trying to load"+filename);
     }
   }
+  var styles = ".w-wrapper{font-family:Helvetica Neue;font-size:16px;max-width:600px;margin:auto;position:fixed;left:0;right:0;top:0;z-index:1000;background-color:white;-webkit-animation:wSlideTopIn 0.3s ease-out;animation:wSlideTopIn 0.3s ease-out}.w-wrapper *{box-sizing:border-box}.w-wrapper a{color:black}.w-wrapper ul{margin:0;-webkit-padding-start:0}.w-wrapper .close{width:30px;display:inline-block;text-align:center;cursor:pointer}.w-wrapper input{width:calc(100% - 30px);font-size:18px;padding:8px 5px}.w-wrapper #libraries{max-height:500px;overflow:auto}.w-wrapper .lib{list-style:none;width:100%;border-bottom:1px #0074D9 solid}.w-wrapper .lib:before,.w-wrapper .lib:after{content:none}.w-wrapper .lib.added .header{background-color:#3D9970}.w-wrapper .header{padding:5px;background-color:#001f3f;color:white;cursor:pointer}.w-wrapper .header:hover{background-color:#00060c}.w-wrapper .name{display:inline-block}.w-wrapper .add-library,.w-wrapper .add{background-color:#2ECC40}.w-wrapper .add{padding:3px 6px;margin-right:5px}.w-wrapper .more-info{background-color:#7FDBFF}.w-wrapper .button,.w-wrapper .add-library,.w-wrapper .more-info{text-transform:uppercase;display:inline-block;font-size:0.8em;font-weight:bold;cursor:pointer}.w-wrapper .add-library,.w-wrapper .more-info{padding:0px 10px;float:right;margin-right:5px}.w-wrapper .more{display:none;padding:5px;background:#0074D9}.w-wrapper .desc{display:inline-block;vertical-align:top;width:57%}.w-wrapper .sidebar{display:inline-block;width:40%}.w-wrapper .homepage{margin-bottom:10px}.w-wrapper .homepage a{color:#7FDBFF}.w-wrapper .homepage strong{display:block;color:black}.w-wrapper .files{background:#AAAAAA;max-height:350px;overflow:auto}.w-wrapper .files .files-header{background:#909090;padding:5px}.w-wrapper .files .file{display:block;padding:5px 5px}@-webkit-keyframes wSlideTopIn{from{-webkit-transform:translateY(-100%);transform:translateY(-100%)}to{-webkit-transform:translateY(0%);transform:translateY(0%)}}@keyframes wSlideTopIn{from{-webkit-transform:translateY(-100%);transform:translateY(-100%)}to{-webkit-transform:translateY(0%);transform:translateY(0%)}}";
+  var stylesElem = document.createElement('style');
+  stylesElem.innerHTML = styles;
+  document.querySelector('head').appendChild(stylesElem);
+
 });
 
